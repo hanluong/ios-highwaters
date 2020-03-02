2d71350304bd4114dabcb303d65824240194368e
LuongHan <hanluongbich@gmail.com>
2020-03-03 00:54:37 +0700

    section 2 - finished

--- HighWatersLab/ViewController.swift
+++ HighWatersLab/ViewController.swift
@@ -7,14 +7,65 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    // Outlets
+    @IBOutlet weak var mapView: MKMapView!
+    
+    // Vars
+    private var locationManager: CLLocationManager!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        locationManager.requestWhenInUseAuthorization()
+        
+//        mapView.showsUserLocation = true
+        mapView.delegate = self
+        
+        locationManager.startUpdatingLocation()
+        
+        setupUI()
+    }
+    
+    private func setupUI() {
+        let addFloodButton = UIButton(frame: .zero)
+        addFloodButton.setImage(UIImage(named: "plus-image"), for: .normal)
+        addFloodButton.addTarget(self, action: #selector(addFloodButtonWasPressed(_:)), for: .touchUpInside)
+        addFloodButton.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(addFloodButton)
+        
+        addFloodButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        addFloodButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -40).isActive = true
+        addFloodButton.widthAnchor.constraint(equalToConstant: 45).isActive = true
+        addFloodButton.heightAnchor.constraint(equalToConstant: 45).isActive = true
+    }
+    
+    @objc func addFloodButtonWasPressed(_ sender: Any) {
+        if let location = locationManager.location {
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = location.coordinate
+            mapView.showsUserLocation = false
+            mapView.addAnnotation(annotation)
+        }
+        print("addFloodButtonWasPressed()")
     }
 
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            let coordinate = location.coordinate
+            let span = MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
+            let region = MKCoordinateRegion(center: coordinate, span: span)
+            mapView.setRegion(region, animated: true)
+            mapView.showsUserLocation = true
+        }
+    }
 }
 
